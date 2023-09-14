70f8cfd0a08d983f20caf349018cd6651820ec1c
Mashkin E.V <mev.86@list.ru>
2023-09-14 23:35:08 +0300

    modified:   element-search/popups/task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,17 @@
+let modalMain = document.getElementById('modal_main')
+modalMain.className += ' modal_active'
+
+let showSuccess = document.querySelector('.show-success')
+let modalSuccess = document.getElementById('modal_success')
+showSuccess.onclick = () => {
+	modalSuccess.className += ' modal_active'
+}
+
+let modalClose = document.querySelectorAll('.modal__close.modal__close_times')
+modalCloses = Array.from(modalClose).forEach(element => {
+	element.onclick = () => {
+		modalSuccess.className = 'modal modal__close'
+		modalMain.className = 'modal modal__close'
+	}
+	
+});
