3c6d3a77838cca42d8657e2d2d55f7fd52123f7c
Mashkin E.V <mev.86@list.ru>
2023-10-06 11:45:00 +0300

    todo completed

---
+++ document-structure/todo/task.js
@@ -0,0 +1,54 @@
+const input = document.getElementById('task__input');
+const btnAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+let returnObj = JSON.parse(localStorage.getItem("myKey")) || [];
+
+function insertHtml(params) {
+	const html = `
+	<div class="task">
+		<div class="task__title">
+			${params}
+		</div>
+		<a href="#" class="task__remove">&times;</a>
+	</div>
+	`;
+	tasksList.insertAdjacentHTML("afterbegin", html);
+};
+
+function localStorageSave(params) {
+	localStorage.setItem("myKey", JSON.stringify(returnObj));
+}
+
+for (const iterator of returnObj) {
+	insertHtml(iterator);
+};
+
+tasksList.addEventListener('click', (e) => {
+	if (e.target.classList.contains('task__remove')) {
+		e.preventDefault();	
+		let taskTitle = e.target.parentNode.querySelector('.task__title').textContent.trim();
+		let index = returnObj.indexOf(taskTitle);
+
+		if (index !== -1) {
+			returnObj.splice(index, 1);
+		};
+
+		localStorageSave();
+
+		e.target.parentNode.remove();
+	};
+});
+
+btnAdd.addEventListener('click', (e) => {
+	e.preventDefault()
+
+	if (input.value.trim() !== '') {
+		insertHtml(input.value);
+	};
+
+	returnObj.push(input.value);
+	input.value = '';
+
+	localStorageSave();
+});
